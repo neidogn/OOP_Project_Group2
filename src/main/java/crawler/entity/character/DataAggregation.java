21a14e79894c0c84c6da056ebcab89c5ce9eaf68
neidogn <dienngo616@gmail.com>
2023-07-10 23:34:01 +0700

    last commit
    Co-Authored-By: Đặng Nam Cường_20215183 <117965430+21tuoino12trieu@users.noreply.github.com>
    Co-Authored-By: CptShox <117516480+CptShox@users.noreply.github.com>
    Co-Authored-By: hieucopy <118492531+hieucopy@users.noreply.github.com>
    Co-Authored-By: luuenhi <117792506+luuenhi@users.noreply.github.com>

--- src/main/java/crawler/entity/character/DataAggregation.java
+++ src/main/java/crawler/entity/character/DataAggregation.java
@@ -25,7 +25,7 @@ public class DataAggregation {
     }
 	public List<Character> loadDataJsonVanSu() throws IOException {
         Gson gson = new Gson();
-        Reader reader = Files.newBufferedReader(Paths.get("OOP_Project_Group2/src/main/java/json/characterVanSu.json"));
+        Reader reader = Files.newBufferedReader(Paths.get("src/main/java/json/characterVanSu.json"));
         List<Character> VanSu = Arrays.asList(gson.fromJson(reader, Character[].class)); 
         reader.close();
 
